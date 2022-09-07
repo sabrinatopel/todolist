17a74c795ffe73b6e63b1a8096db98fff9ade5cd
sabrinatopel <sabrina.f.topel@gmail.com>
2022-09-07 16:40:34 -0300

    save the estructure again

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from pickle import TRUE
-from flask import Flask
+from turtle import title
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db =SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 
 @app.route("/")
 def index():
-    return "Hello World"
-
-@app.route("/about")
-def about():
-    return "About"
+    # show all todos
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template("base.html")
 
 if __name__ == "__main__":
-    app.runt(debug=TRUE)
+    db.create_all()
+
+    new_todo = Todo(title="todo 1", complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+
+    app.runt(debug=True)
